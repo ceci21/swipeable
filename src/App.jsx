a0f0bbc008632740ab08361c1a63d722d411f945
ceci21 <ceci.goss@gmail.com>
2021-03-31 21:45:00 -0500

    styling

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Swipeable from './Swipeable';
+import './App.css'
+
+const App = () => {
+  return (
+    <div class="app">
+      <Swipeable />
+    </div>
+  );
+};
+
+export default App;
--- src/Swipeable.jsx
+++ src/Swipeable.jsx
@@ -21,54 +21,76 @@ import './Swipeable.scss';
 const example = [
   {
     content: 'A',
-    stop: function() { console.log(this.content) }
+    stop: function () {
+      console.log(this.content);
+    },
   },
   {
     content: 'B',
-    stop: function() { console.log(this.content) }
+    stop: function () {
+      console.log(this.content);
+    },
   },
   {
     content: 'C',
-    stop: function() { console.log(this.content) }
+    stop: function () {
+      console.log(this.content);
+    },
   },
   {
     content: 'D',
-    stop: function() { console.log(this.content) }
+    stop: function () {
+      console.log(this.content);
+    },
   },
-]
+];
 
 const Swipeable = ({ width = 500, elements = example }) => {
   const approxTargetWidth = Math.floor(width / elements.length + 1);
   const approxElLocations = [];
 
-
   useEffect(() => {
     $('.swiper').draggable({
       axis: 'x',
       containment: 'parent',
-      scroll: false,
+      scroll: true,
       snap: '.target',
-      snapTolerance: 45,
-      stop: function(_, stats) {
-
-        console.log(stats)
+      snapTolerance: 50,
+      stop: function (_, stats) {
+        console.log(stats);
         const i = Math.floor(stats.position.left / approxTargetWidth);
-        console.log(elements[i].stop.bind(elements[i])())
-      }
-
+        if (i > 0) {
+          console.log(elements[i].stop.bind(elements[i])());
+        }
+        $('.swiper').addClass('fall');
+        setTimeout(() => {
+          $('.swiper').css('left', 0);
+          setTimeout(() => {
+            $('.swiper').removeClass('fall');
+            $('.swipeable').removeClass('wiggle');
+          }, 1000)
+        }, 100);
+      },
     });
   }, []);
 
   return (
     <div className="swipeable">
       <div className="swiper draggable ui-widget-content">X</div>
-      {
-        elements.map(({ content, stop }) => {
+      {/* <div className="home target">
+        h
+      </div> */}
+      {elements.map(({ content, stop }) => {
+        if (content) {
           return (
-            <div className="target" onClick={stop}>{ content }</div>
-          )
-        })
-      }
+            <div className="target" onClick={stop}>
+              {content}
+            </div>
+          );
+        } else {
+          return null;
+        };
+      })}
     </div>
   );
 };
